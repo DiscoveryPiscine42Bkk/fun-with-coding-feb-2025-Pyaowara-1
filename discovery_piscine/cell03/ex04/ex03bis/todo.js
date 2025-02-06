9f620b35139da0d5253270adffb8f81ee1be25a4
loleo <pattrapol.nope@gmail.com>
2025-02-06 19:16:33 +0700

    c3e4f

--- discovery_piscine/cell03/ex04/ex03bis/todo.js
+++ discovery_piscine/cell03/ex04/ex03bis/todo.js
@@ -10,9 +10,10 @@ function createTodo(text, id) {
 
 function update() {
     list.empty();
-    for (let item of TODO)
+    for (let item of TODO) {
         list.append(createTodo(item.text, item.id));
-    document.cookie = "todos=" + JSON.stringify(TODO) + ";path=/";
+    }
+    $.cookie('todos', JSON.stringify(TODO), { path: '/' });
 }
 
 function newTodo() {
@@ -30,23 +31,10 @@ function remove(id) {
     }
 }
 
-$(document).ready(function () {
-    const cookies = document.cookie.split(';');
-    let todoCookie = null;
-
-    for (const cookie of cookies) {
-        const [name, value] = cookie.trim().split('=');
-        if (name === 'todos') {
-            todoCookie = value;
-            break;
-        }
-    }
-
-    if (todoCookie) {
-        TODO = JSON.parse(todoCookie);
-        nextId = TODO.length > 0 ? Math.max(...TODO.map(item => item.id)) + 1 : 0;
-        console.log(TODO.map(item => item.id));
-        update();
-    }
-}
-);
+const todoCookie = $.cookie('todos');
+    
+if (todoCookie) {
+    TODO = JSON.parse(todoCookie);
+    nextId = TODO.length > 0 ? Math.max(...TODO.map(item => item.id)) + 1 : 0;
+    update();
+};
